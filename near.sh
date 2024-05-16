096f2a57057ea2ff0278e09b61bb73296f300405
Frodo <83783418+frodobigens@users.noreply.github.com>
2024-05-17 00:31:18 +0300

    Update near.sh

--- near.sh
+++ near.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-RPC_URL="https://near.lava.build/lava-referer-8b51600b-b3cc-4893-89ec-5de41f0724db/"
+RPC_URL="https://near.lava.build/lava-referer-390d1a06-db37-40a9-b4a8-81a33d0ea6c6/"
 
 OUTPUT_DIR="logs"
 
-near_data=$(curl -s -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"block","id":"HappyCuanAirdrop","params":{"finality":"final"}}' "$RPC_URL")
+UA=$(shuf -n 1 ua.txt)
+
+near_data=$(curl -A UA -x http://154.92.125.104:5405 --proxy-user cwmgurpo:3lv7ii9r7e6p -s -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"block","id":"HappyCuanAirdrop","params":{"finality":"final"}}' "$RPC_URL")
 
 if [ $? -eq 0 ]; then
     if [ ! -d "$OUTPUT_DIR" ]; then
